ce7f6cfc1425cfc68fc86c563ec45687625a0e9c
ducanhvina <ducanhvina17@gmail.com>
2018-12-29 10:47:01 +0700

    final_1

--- JPEG_ENCODER/test.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-with open("out.txt", "rb") as inputt:
-    inputt=(inputt.read())
-    with open(b"someobject.pickle", "wb") as output_file:
-        pickle.dump([inputt,inputt],output_file)
